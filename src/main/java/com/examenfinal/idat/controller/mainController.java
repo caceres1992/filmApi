f3380834c94523d006b46817b65144ff9c4b7197
jasson <jassoncaceres98@gmail.com>
2020-08-11 22:32:18 -0500

    2do ejmplo

---
+++ src/main/java/com/examenfinal/idat/controller/mainController.java
@@ -0,0 +1,16 @@
+package com.examenfinal.idat.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+@RequestMapping
+public class mainController {
+    @GetMapping
+    public String Message() {
+        return "Hello todo esta funcionando";
+    }
+}
